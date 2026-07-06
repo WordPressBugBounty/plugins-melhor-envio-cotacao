ad281194c292bd280d8a6d68953e63adc2f9c45f
Sean Murphy <sean@iamseanmurphy.com>
2026-07-06 13:00:08 +0000

    2.16.5

--- melhor-envio-cotacao/vendor/composer/installed.php
+++ melhor-envio-cotacao/vendor/composer/installed.php
@@ -1,9 +1,9 @@
 <?php return array(
     'root' => array(
         'name' => 'melhorenvio/melhorenvio-cotation',
-        'pretty_version' => 'v2.16.4',
-        'version' => '2.16.4.0',
-        'reference' => '5054e30d82c390b36dc65b1694d8e80d602d3437',
+        'pretty_version' => 'v2.16.5',
+        'version' => '2.16.5.0',
+        'reference' => 'fa50fd2c2549ffd256eae3612b88413aef2753f6',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,9 +11,9 @@
     ),
     'versions' => array(
         'melhorenvio/melhorenvio-cotation' => array(
-            'pretty_version' => 'v2.16.4',
-            'version' => '2.16.4.0',
-            'reference' => '5054e30d82c390b36dc65b1694d8e80d602d3437',
+            'pretty_version' => 'v2.16.5',
+            'version' => '2.16.5.0',
+            'reference' => 'fa50fd2c2549ffd256eae3612b88413aef2753f6',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
